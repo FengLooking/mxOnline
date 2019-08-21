9366f226dcf4f27fc10f8330d2c828279eae6d4d
丁义峰 <dingyifeng@dingyifengs-MBP.lan>
2019-08-21 20:47:09 +0800

    course organizations data show

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,12 +5,26 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from .models import CityDict
+from .models import CourseOrg
+from .models import Teacher
 # Create your views here.
 
 
 class OrgView(View):
     """
-    课程机构列表
+    function: 课程机构列表
     """
     def get(self, request):
-        return render(request, 'org-list.html', {})
+        # 课程机构
+        all_orgs = CourseOrg.objects.all()
+        org_nums = all_orgs.count()
+        print('all nums is: %s' % org_nums)
+        # 城市
+        all_citys = CityDict.objects.all()
+    
+        return render(request, 'org-list.html', {
+            'all_orgs':all_orgs,
+            'all_citys':all_citys,
+            'org_nums':org_nums
+        })
