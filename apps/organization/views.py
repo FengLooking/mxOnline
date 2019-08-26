b1874050b53157f170946206be5d3fcca6c04811
丁义峰 <dingyifeng@dingyifengs-MBP.lan>
2019-08-26 22:25:50 +0800

    category and sort

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -20,10 +20,29 @@ class OrgView(View):
     def get(self, request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
-        print('all nums is: %s' % org_nums)
+        hot_orgs = all_orgs.order_by('-click_nums')[:3]
         # 城市
         all_citys = CityDict.objects.all()
+        # 取出筛选城市
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 机构类别筛选
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+        
+        # 排名
+        sort = request.GET.get('sort', '')
+        if sort:
+            if sort == 'students':
+                all_orgs = all_orgs.order_by('-students')
+            elif sort == 'courses':
+                all_orgs = all_orgs.order_by('-courses_nums')
+
+        org_nums = all_orgs.count()
+        print('all nums is: %s' % org_nums)
 
         # 对课程机构进行分页
         try:
@@ -38,5 +57,9 @@ class OrgView(View):
         return render(request, 'org-list.html', {
             'all_orgs':orgs,
             'all_citys':all_citys,
-            'org_nums':org_nums
+            'org_nums':org_nums,
+            'city_id': city_id,
+            'category': category,
+            'hot_orgs': hot_orgs,
+            'sort': sort
         })
